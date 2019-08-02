8954e53f279b1387ce7923e88d1acadb92b92e61
Roberto Franks <francisavp@hotmail.com>
2019-08-01 20:43:41 -0600

    Lab 2

---
+++ src/Detail.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+
+class Detail extends React.Component {
+  render() {
+    return (
+      <h2>
+          Detail Title: {this.props.detailTitle}, Detail Value: {this.props.detailValue}
+      </h2>
+    )
+  }
+}
+
+Detail.propTypes = {
+    detailTitle: PropTypes.string,
+}
+
+Detail.defaultProps = {
+    detailTitle: 'Welcome',
+    detailValue: 2
+}
+
+export default Detail
---
+++ src/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Detail from './Detail'
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { title: "Laboratorio" }
+    this.update = this.update.bind(this)
+  }
+
+  update() {
+    this.setState({ title: "Aplicaciones de Vanguardia" })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{ this.state.title }<br></br></h1>
+        <button onClick = { this.update }>Revert.</button>
+        <Detail detailTitle = {this.title} detailValue = {3}/>
+      </div>
+    )
+  }
+}
+
+export default Home
